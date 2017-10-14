9cffe9f3fe9c50136f9811fd00b8f3b439c59709
Nic Cheneweth <nchenewe@thoughtworks.com>
2017-10-14 14:22:49 -0500

    update config.yml

---
+++ setup_profile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir ~/.aws
+cat <<EOF > ~/.aws/credentials
+[bootstrap-profile]
+aws_access_key_id=$profile_access_key
+aws_secret_access_key=$profile_secret_key
+region=$profile_region
+EOF

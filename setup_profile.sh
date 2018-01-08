2c7ddd06d61492efb225ed4874974c95dc4926f0
ncheneweth <nchenewe@thoughtworks.com>
2018-01-08 11:13:17 -0600

    update profile defintion

--- setup_profile.sh
+++ setup_profile.sh
@@ -2,7 +2,7 @@
 
 mkdir ~/.aws
 cat <<EOF > ~/.aws/credentials
-[bootstrap-profile]
+[default]
 aws_access_key_id=$profile_access_key
 aws_secret_access_key=$profile_secret_key
 region=$profile_region

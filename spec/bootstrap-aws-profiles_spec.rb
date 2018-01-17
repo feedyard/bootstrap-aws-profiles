38d2ee4fbecafe8d046d2583093e2b331948c908
ncheneweth <nchenewe@thoughtworks.com>
2018-01-16 21:19:09 -0500

    update testing

--- spec/bootstrap-aws-profiles_spec.rb
+++ spec/bootstrap-aws-profiles_spec.rb
@@ -9,3 +9,9 @@ end
 describe iam_group('AssumeProdRoleGroup') do
   it { should exist }
 end
+
+describe iam_group('UseParameterStoreGroup') do
+  it { should exist }
+  it { should have_iam_policy('UseParameterStorePolicy') }
+  it { should be_allowed_action('ssm:DescribeParameters').resource_arn('arn:aws:ssm:us-east-1:667882779648:*') }
+end

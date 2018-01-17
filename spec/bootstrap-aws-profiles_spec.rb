ebffda714834199b604a19b2838372b896cf6500
ncheneweth <nchenewe@thoughtworks.com>
2018-01-16 21:40:39 -0500

    update testing

--- spec/bootstrap-aws-profiles_spec.rb
+++ spec/bootstrap-aws-profiles_spec.rb
@@ -4,14 +4,15 @@ require 'spec_helper'
 
 describe iam_group('AssumeNonprodRoleGroup') do
   it { should exist }
+  # it { should have_inline_policy('UseParameterStorePolicy') }
 end
 
 describe iam_group('AssumeProdRoleGroup') do
   it { should exist }
+  # it { should have_inline_policy('UseParameterStorePolicy') }
 end
 
 describe iam_group('UseParameterStoreGroup') do
   it { should exist }
   it { should have_inline_policy('UseParameterStorePolicy') }
-  it { should be_allowed_action('ssm:DescribeParameters') }
 end
